b0d8228c8e4a73fcd18ba56f9644033cfaa67b12
nikhil-500 <nikhil@appup.com>
2019-03-04 15:10:56 +0530

    Initial commit

---
+++ src/main/java/com/nikhil/main/SikshagramApplication.java
@@ -0,0 +1,13 @@
+package com.nikhil.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SikshagramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SikshagramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nikhil/main/SikshagramApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nikhil.main;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SikshagramApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
